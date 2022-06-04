8cb6879b0ba2b5d846164bc5cff5b93786aa54ab
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-06-05 00:03:44 +0600

    drop down functionality added

--- src/Component/Products/ProductColors.js
+++ src/Component/Products/ProductColors.js
@@ -1,15 +1,23 @@
 import ProductColor from './ProductColor';
-
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid'
+import { useState } from 'react';
 const ProductColors = ({ state }) => {
+
+    const [isTabOpen, setIsTabOpen] = useState(true)
 
 
     const skuColor = state?.colors?.find(color => color?.id === state?.selectedSku?.props[0])
 
     return (
         <div className='py-2 px-4 bg-white mt-6'>
-            <h3 className='text-gray-500 font-medium text-xl'>Color: <span className='text-black'> {skuColor?.name}</span></h3>
+            <div className='flex items-center justify-between'>
+                <h3 className='text-gray-500 font-medium text-xl'>Color: <span className='text-black'> {skuColor?.title}</span> </h3>
+                {isTabOpen
+                    ? <ChevronUpIcon onClick={() => setIsTabOpen(false)} className="h-5 w-5 text-gray-500" />
+                    : <ChevronDownIcon onClick={() => setIsTabOpen(true)} className="h-5 w-5 text-gray-500" />}
+            </div>
 
-            <div className='flex flex-wrap gap-6 mt-4'>
+            <div className={`flex flex-wrap gap-6 mt-4 ${isTabOpen ? 'block' : 'hidden'}`}>
                 {
                     state?.colors?.map(color => <ProductColor key={color.id} color={color} />)
                 }
--- src/Component/Products/ProductSizes.js
+++ src/Component/Products/ProductSizes.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/solid';
+import React, { useState } from 'react';
 import ProductSize from './ProductSize';
 
 const ProductSizes = ({ state }) => {
+
+    const [isTabOpen, setIsTabOpen] = useState(true)
+
     const skuSize = state?.sizes?.find(size => size?.id === state?.selectedSku?.props[1])
 
     return (
         <div className='px-4 py-2 bg-white mt-6'>
-            <h3 className='text-gray-500 font-medium text-xl'>Size: <span className='text-black'> {skuSize?.name}</span></h3>
 
-            <div className='grid grid-cols-5 gap-4 mt-4'>
+            <div className='flex items-center justify-between'>
+                <h3 className='text-gray-500 font-medium text-xl'>Size: <span className='text-black'> {skuSize?.name}</span></h3>
+                {isTabOpen
+                    ? <ChevronUpIcon onClick={() => setIsTabOpen(false)} className="h-5 w-5 text-gray-500" />
+                    : <ChevronDownIcon onClick={() => setIsTabOpen(true)} className="h-5 w-5 text-gray-500" />}
+            </div>
+
+            <div className={`grid grid-cols-5 gap-4 mt-4 ${isTabOpen ? 'block' : 'hidden'}`}>
                 {
                     state?.sizes?.map(size => <ProductSize key={size.id} size={size} />)
                 }
